c415f2df352583d92a31e459c3a63e394dda9c49
bennyroses <beatrice.stumpo@studio.unibo.it>
2024-06-05 11:11:18 +0200

    Add files via upload

--- File.hpp
+++ File.hpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <ncurses.h>
+
 using namespace std;
 
 struct node {
@@ -8,5 +11,99 @@ struct node {
 
 typedef node* plist;
 
-void saveData(int points);
-void loadData();
+plist addNode(int points, plist head) {
+  if (head == NULL) {
+    head = new node;
+    head->val = points;
+    head->next = NULL;
+    return head;
+  }
+
+  bool added = false;
+  plist curr = head;
+
+  while (!added && curr != NULL) {
+    if (curr->val < points) {
+      plist tmp = curr->next;
+      curr->next = new node;
+      (curr->next)->val = points;
+      (curr->next)->next = tmp;
+      added = true;
+    }
+
+    curr = curr->next;
+  }
+
+  if (!added) {
+    plist n = new node;
+    n->val = points;
+    n->next = head;
+    head = n;
+  }
+
+  return head;
+}
+
+void save_data(int points) {
+  ofstream file;
+  file.open("classifica.txt", ios::app);
+
+  file << points;
+  file.close();
+}
+
+plist load_data(plist head) {
+  ifstream file;
+  file.open("classifica.txt");
+  int points;
+  
+  while (file >> points) {
+    head = addNode(points, head);
+  }
+  return head;
+}
+
+void ranking(){
+    initscr();
+    cbreak();
+    noecho();
+    curs_set(0);
+    int maxy, maxx;
+    getmaxyx(stdscr, maxy, maxx);
+    plist head =NULL;
+    head = load_data(head);
+    WINDOW *classifica= newwin(21, 30, maxy/4, maxx/4);
+    box(classifica, 0,0);
+    refresh();
+    wrefresh(classifica);
+    keypad(classifica, true);
+    mvwaddch(classifica, 3, 2, 9);
+    int p[10];
+    int r[9]={1,2,3,4,5,6,7,8,9};
+    int i=0;
+     
+    int e=1;
+    int a;
+    for (int i = 0; i < 9; i++)    //stampa un elenco di numeri da 1 a 10
+      {
+        mvwprintw(classifica, e, 2, "%d", r[i]);
+        mvwaddch(classifica, e, 3, '.');
+        e=e+2;
+      }
+    mvwprintw(classifica, 19,2, "10");
+    mvwaddch(classifica, 19,4, '.');
+    e=1;
+    while(head!=NULL & i<10) //inserisci i primi 10 punteggi in classifica
+    {
+      mvwprintw(classifica, e,6,"%d", head->val);
+      head=head->next;
+      i++;
+      e= e+2;
+    }
+    do{
+      wrefresh(classifica);
+      a=wgetch(classifica);
+    }while (a!=10);
+    
+    endwin();
+}
