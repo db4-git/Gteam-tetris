68ccb35fd4e66468b076607603307889f32ffc55
CesidioR <161586794+CesidioR@users.noreply.github.com>
2024-06-06 17:54:21 +0200

    manca aggiungiere il gioco

--- Tetrisgame.hpp
+++ Tetrisgame.hpp
@@ -2,6 +2,7 @@
 //#include "Griglia.hpp"
 #include <iostream>
 #include "menu.hpp"
+#include "griglia.hpp"
 //#include "file.hpp"
 
 class TetrisGame{
@@ -9,8 +10,8 @@ class TetrisGame{
     //Griglia griglia;
     int xmax, ymax;
     bool game_over;
-    Game_Menu *menu;
-    Ranking *classifica;
+    //Game_Menu *menu;
+    //Ranking *classifica;
     public:
     TetrisGame(){
         initscr();
@@ -19,9 +20,47 @@ class TetrisGame{
         curs_set(0);
         int sce;
         getmaxyx(stdscr, ymax, xmax);
+     
+        
         int highlight=0;
-        //create_menu(ymax, xmax);
-        menu = new Game_Menu(ymax, xmax, highlight);
+        //menu = new Game_Menu(ymax, xmax, highlight, sce);
+        //create_griglia();
+        create_menu(ymax, xmax, highlight, sce);
+      
+        
+            while (1)
+            {
+                 if (highlight==0 && sce == 10)
+            {
+                create_griglia();
+                
+            }
+            if (highlight==1 && sce == 10)
+            {
+                create_ranking(ymax, xmax, sce);
+                if (sce == 'q')
+                {
+                    create_menu(ymax, xmax, highlight, sce);
+                }
+                
+            }
+            }
+            //create_menu(ymax, xmax, highlight, sce);
+            //sce=getch();
+            //create_menu(ymax, xmax, highlight, sce);
+            /*if (highlight==0 && sce == 10)
+            {
+                create_griglia();
+            }
+            if (highlight==1 && sce == 10)
+            {
+                create_ranking(ymax, xmax);
+                refresh();
+            }
+            */
+
+        //create_griglia();
+        
         getch();
         endwin();
     }
