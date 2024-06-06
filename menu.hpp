f40dc171f21b7fdddf1ba01facefffd06f9a72b9
CesidioR <161586794+CesidioR@users.noreply.github.com>
2024-06-06 09:29:03 +0200

    crea il menu
    Sono presenti una funzione e una classe che fanno la stessa cosa vedete voi quale preferite, col menu e gia unita la classifica

--- menu.hpp
+++ menu.hpp
@@ -1,5 +1,5 @@
 #include <ncurses.h>
-#include "file.hpp"
+#include "ranking.hpp"
 using namespace std;
 struct scelte
 {
@@ -10,20 +10,19 @@ class Game_Menu
 {
     protected:
     WINDOW *menuwin;
+    Ranking *classifica;
 public:
-    Game_Menu(int ymax,int xmax){
+    Game_Menu(int ymax,int xmax, int highlight){
     menuwin= newwin(4, 20, ymax / 2, xmax / 2);
     box(menuwin, 0,0);
     refresh();
     wrefresh(menuwin);
     keypad(menuwin, true);
-
-     int highlight=0;
-    int choice;
+    //int highlight=0;
     scelte o[2]={{" NUOVA PARTITA "},{"  CLASSIFICA  "}};
-    
+    int choice;
     wrefresh(menuwin);
-
+    
     while (1)
     {
        
@@ -61,19 +60,26 @@ public:
             default:
                 break;
         }
+        
         if (highlight== 1 & choice == 10)
         {
-            ranking();
-        }
-        if (choice== 'q'){
-            delwin(menuwin);
-            clear();
+            
+            //wclear(menuwin);
+            //wrefresh(menuwin);
+            //delwin(menuwin);
+            classifica= new Ranking(ymax, xmax);
+            
         }
         
-        if (choice == 10) //10 è il valore di del tasto invio
+        
+        if (choice == 10  && highlight==0) //10 è il valore di del tasto invio
         {
-           break;
+            wclear(menuwin);
+            wrefresh(menuwin);
+            delwin(menuwin);
+            break;
         }
+       
         
     }
     }
@@ -81,7 +87,74 @@ public:
 
 };
 
+void create_menu(int  ymax,int xmax){
 
+    WINDOW *menu= newwin(4, 20, ymax/2, xmax/2);
+    box(menu, 0,0);
+    refresh();
+    wrefresh(menu);
+    keypad(menu, true);
+
+
+    int highlight=0;
+    int choice;
+    scelte o[2]={{" NUOVA PARTITA "},{"  CLASSIFICA  "}};
+    
+    wrefresh(menu);
+
+    while (1)
+    {
+       
+        for(int i = 0; i < 2; i++)
+        {
+            //vogliamo evidenziare cosa stiamo facendo ora
+            if (i == highlight)
+            {
+                //attron per una specifica finestra
+                wattron(menu, A_STANDOUT);
+            }
+            mvwprintw(menu,i+1, 3, o[i].a);
+            
+            wattroff(menu,A_STANDOUT);
+        }
+        
+        choice=wgetch(menu);
+
+        switch (choice)
+        {
+            case KEY_UP:
+                highlight--;
+                if (highlight == -1)
+                {
+                    highlight=1;
+                }
+                break;
+            case KEY_DOWN:
+                highlight++;
+                if (highlight==2)
+                {
+                    highlight=0;
+                }
+                break;
+            default:
+                break;
+        }
+        if (highlight==1 && choice==10)
+        {
+            create_ranking(ymax, xmax);
+        }
+        
+        if (choice == 10 && highlight==0) //10 è il valore di del tasto invio
+        {
+            wclear(menu);
+            wrefresh(menu);
+            delwin(menu);
+        }
+        
+    }
+    
+
+}
 
 
 
