b4f83354c4d567e8cb6768f1822ba9dc0bf64120
CesidioR <161586794+CesidioR@users.noreply.github.com>
2024-06-06 09:30:31 +0200

    crea la classifica
    sono presenti una classe e una funzione che fanno la stessa cosa vedete voi quale preferite, in questo file ci sono anche le funzioni che servono a salvare i file

--- ranking.hpp
+++ ranking.hpp
@@ -114,3 +114,50 @@ class Ranking{
 
   }
 };
+
+
+void create_ranking(int ymax,int xmax){
+plist head =NULL;
+      head = load_data(head);
+      WINDOW *rank= newwin(21, 30, ymax/4, xmax/4);
+      box(rank, 0,0);
+      refresh();
+      wrefresh(rank);
+      refresh();
+      keypad(rank, true);
+      mvwaddch(rank, 3, 2, 9);
+      int r[9]={1,2,3,4,5,6,7,8,9};
+      int i=0;
+     
+      int e=1;
+      int a;
+      for (int i = 0; i < 9; i++)    //stampa un elenco di numeri da 1 a 10
+        {
+          mvwprintw(rank, e, 2, "%d", r[i]);
+          mvwaddch(rank, e, 3, '.');
+          e=e+2;
+        }
+      mvwprintw(rank, 19,2, "10");
+      mvwaddch(rank, 19,4, '.');
+      e=1;
+      while(head!=NULL & i<10) //inserisci i primi 10 punteggi in classifica
+      {
+        mvwprintw(rank, e,6,"%d", head->val);
+        head=head->next;
+        i++;
+        e= e+2;
+      }
+      do //refresha continuamente e la tiene bloccata a schermo
+      {
+        wrefresh(rank);
+        a=wgetch(rank);
+      } while (a!='q');
+    
+    if (a =='q') //elimina la finestra e la cancella dallo schermo
+    {
+      wclear(rank);
+      wrefresh(rank);
+      delwin(rank);
+    }
+
+  }
